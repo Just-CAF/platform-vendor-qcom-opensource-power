95b8962134eb0b8ce580800d54872b7344b277aa
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:39 -0700

    power: Add support for tap-to-wake feature control
    Change-Id: I545902b29f4828c127bc32def6e30b67ce4a3aa7

--- Power.cpp
+++ Power.cpp
@@ -29,8 +29,8 @@
 
 #define LOG_TAG "QTI PowerHAL"
 
-#include <android/log.h>
-#include <utils/Log.h>
+#include <android-base/file.h>
+#include <log/log.h>
 #include "Power.h"
 #include "power-common.h"
 
@@ -65,6 +65,15 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
 }
 
 Return<void> Power::setFeature(Feature feature, bool activate)  {
+    switch (feature) {
+#ifdef TAP_TO_WAKE_NODE
+        case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
+            ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE);
+            break;
+#endif
+        default:
+            break;
+    }
     return Void();
 }
 
